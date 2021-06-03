a1a621b6da4cb59ff66409008f5e6fc664470afe
Berkan Uslu <berkanuslu@gmail.com>
2021-06-03 02:41:38 +0200

    UPDATED RootPath implementation by using DEPLOYMENT_BUILD_NUMBER instead of using DEPLOYMENT_BRANCH_NAME for non-master and non-development branches.

--- services/SchedulerService/Program.cs
+++ services/SchedulerService/Program.cs
@@ -54,7 +54,7 @@ namespace SchedulerService
             var RootPath = "/";
             if (ServInit.RequiredEnvironmentVariables["DEPLOYMENT_BRANCH_NAME"] != "master" && ServInit.RequiredEnvironmentVariables["DEPLOYMENT_BRANCH_NAME"] != "development")
             {
-                RootPath = "/" + ServInit.RequiredEnvironmentVariables["DEPLOYMENT_BRANCH_NAME"] + "/";
+                RootPath = "/" + ServInit.RequiredEnvironmentVariables["DEPLOYMENT_BUILD_NUMBER"] + "/";
             }
 
             /*
